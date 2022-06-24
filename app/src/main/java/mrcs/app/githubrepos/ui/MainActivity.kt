c475845050a6a49cd35819224ba3de470c7f644f
mrcs22 <marcusmoraes2010@hotmail.com>
2022-06-24 04:03:03 -0300

    project setup

---
+++ app/src/main/java/mrcs/app/githubrepos/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package mrcs.app.githubrepos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mrcs.app.githubrepos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
