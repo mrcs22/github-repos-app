73050ddd2b9eb0c45e40e9ea2a533fe60ab147fd
mrcs22 <marcusmoraes2010@hotmail.com>
2022-06-24 05:41:57 -0300

    feat: add search action to menu

--- app/src/main/java/mrcs/app/githubrepos/ui/MainActivity.kt
+++ app/src/main/java/mrcs/app/githubrepos/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package mrcs.app.githubrepos.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import mrcs.app.githubrepos.R
+import mrcs.app.githubrepos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
 }
